dc3cf5936a917ef54df6e11d096a64808fba25f0
Gang Zhuo <gang.zhuo@gmail.com>
2015-06-09 22:15:03 -0400

    add files

---
+++ Sample/Sample_1_FileBackup/Backup/BackupWorker.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using BaiduPCS_NET;
+
+namespace FileBackup
+{
+    public class BackupWorker : Worker
+    {
+        /// <summary>
+        /// 目前只有 pcs.getRawData() 函数使用，用于接收数据编码
+        /// </summary>
+        private string encode;
+
+        /// <summary>
+        /// 在控制台的 (X, Y) 处显示进度
+        /// </summary>
+        private int X = -1;
+
+        /// <summary>
+        /// 在控制台的 (X, Y) 处显示进度
+        /// </summary>
+        private int Y = -1;
+
+        /// <summary>
+        /// 上传单元
+        /// </summary>
+        public Uploader uploader { get; private set; }
+
+        /// <summary>
+        /// 存储网盘中各文件的元数据
+        /// </summary>
+        public SortedDictionary<string, long> fileList { get; private set; }
+
+        public override string WorkerName
+        {
+            get
+            {
+                return "Backup";
+            }
+        }
+
+        public BackupWorker(BaiduPCS pcs, BackupItem backupItem, string userDir)
+            : base(pcs, backupItem, userDir)
+        {
+
+        }
+
+        protected override void _PreRun()
+        {
+            base._PreRun();
+            fileList = ReadRemoteFileListAsDictionary();
+            uploader = new SecureUploader(pcs, slice_dir, new MyBlockEncoder());
+            uploader.ProgressChange += onProgressChange;
+            uploader.UploadSliceError += onUploadSliceError;
+            uploader.ProgressEnabled = true;
+        }
+
+        protected override void _Run()
+        {
+            WriteLogAndConsole("Backup " + backupItem.LocalPath + " => " + backupItem.RemotePath);
+            WriteLogAndConsole("UID: " + pcs.getUID());
+
+            Console.WriteLine();
+            X = Console.CursorLeft;
+            Y = Console.CursorTop;
+            BackupDirectory(backupItem.LocalPath, backupItem.RemotePath);
+
+            string s = string.Format("Total: {0}, Skip: {1}, Fail: {2}, Succ: {3}, Rename Total: {4}, Rename Fail: {5}, Rename Succ: {6}\r\n\r\n",
+                total, skip, fail, total - skip - fail,
+                rename_total, rename_fail, rename_total - rename_fail);
+            WriteLogAndConsole(s);
+        }
+
+        protected override void _RunCompleted()
+        {
+
+            Uploader uploader = new Uploader(pcs, slice_dir);
+            uploader.UploadSliceError += onUploadSliceError;
+            uploader.ProgressEnabled = false;
+
+            #region 把文件列表记录 上传到网盘
+
+            uploader.ProgressEnabled = false;
+            int x = Console.CursorLeft,
+                y = Console.CursorTop;
+            string filename = Path.GetFileName(list_file);
+            string remote_list_file = backupItem.RemotePath + "/" + ".meta/list/" + filename;
+            WriteConsole("Upload " + list_file);
+            PcsFileInfo pfi = uploader.UploadFile(list_file, remote_list_file, true);
+            if (!pfi.IsEmpty)
+            {
+                WriteLogAndConsole("Upload " + list_file + " Success", y, x);
+            }
+            else
+            {
+                WriteLogAndConsole("Failed to upload " + list_file + ": " + pcs.getError(), y, x);
+                WriteLogAndConsole(pcs.getRawData(out encode));
+            }
+
+            // 设置上传的文件列表文件为最新的文件列表
+            byte[] refBytes = Encoding.UTF8.GetBytes(remote_list_file);
+            x = Console.CursorLeft;
+            y = Console.CursorTop;
+            WriteConsole("Set " + remote_ref_file);
+            pfi = pcs.upload(remote_ref_file, refBytes, true);
+            if (!pfi.IsEmpty)
+            {
+                WriteLogAndConsole("Set " + remote_ref_file + " Success", y, x);
+            }
+            else
+            {
+                WriteLogAndConsole("Failed to set " + remote_ref_file + ": " + pcs.getError(), y, x);
+                WriteLogAndConsole(pcs.getRawData(out encode));
+            }
+
+            #endregion
+
+            #region 上传日志
+
+            filename = Path.GetFileName(log_file);
+            string remote_log_file = backupItem.RemotePath + "/" + ".meta/log/" + filename;
+            x = Console.CursorLeft;
+            y = Console.CursorTop;
+            WriteConsole("Upload log " + log_file);
+            pfi = uploader.UploadFile(log_file, remote_log_file, true);
+            if (!pfi.IsEmpty)
+            {
+                WriteLogAndConsole("Upload log " + log_file + " Success", y, x);
+            }
+            else
+            {
+                WriteLogAndConsole("Failed to upload log " + log_file + ": " + pcs.getError(), y, x);
+                WriteLogAndConsole(pcs.getRawData(out encode));
+            }
+
+            #endregion
+
+            base._RunCompleted();
+        }
+
+        /// <summary>
+        /// 备份该目录
+        /// </summary>
+        /// <param name="localPath"></param>
+        /// <param name="remotePath"></param>
+        protected void BackupDirectory(string localPath, string remotePath)
+        {
+            string filename;
+            string[] dirs = Directory.GetDirectories(localPath);
+            if (dirs != null)
+            {
+                foreach (string dir in dirs)
+                {
+                    filename = Path.GetFileName(dir);
+                    if (filename == ".meta")
+                        continue;
+                    BackupDirectory(dir, remotePath + "/" + filename);
+                }
+            }
+
+            string[] files = Directory.GetFiles(localPath);
+            if (files != null)
+            {
+                foreach (string file in files)
+                {
+                    filename = Path.GetFileName(file);
+                    if (filename.ToLower() == "thumbs.db")
+                        continue;
+                    BackupFile(file, remotePath + "/" + filename);
+                }
+            }
+        }
+
+        protected void BackupFile(string localPath, string remotePath)
+        {
+            total++;
+            PcsFileInfo pfi;
+            FileInfo fi = new FileInfo(localPath);
+
+            //本地文件修改时间
+            long lastModifyTime = Utils.UnixTimeStamp(fi.LastWriteTimeUtc);
+
+            //记录到列表中
+            WriteList(lastModifyTime, remotePath);
+
+            //获取网盘中文件的最后修改时间
+            long remoteLastModifyTime = -1;
+            if (fileList.ContainsKey(remotePath))
+                remoteLastModifyTime = fileList[remotePath];
+
+            //假如本地修改时间 小于等于 网盘修改时间，则无需备份
+            if (remoteLastModifyTime > 0 && lastModifyTime <= remoteLastModifyTime)
+            {
+                skip++;
+                WriteLogAndConsole("Skip " + localPath.Substring(backupItem.LocalPath.Length) + "    ", Y, X);
+                return;
+            }
+
+            WriteConsole("Backup " + localPath.Substring(backupItem.LocalPath.Length) + "    ", Y, X);
+            pfi = uploader.UploadFile(localPath, remotePath, true);
+            pfi = Validate(pfi, remotePath);
+            if (!pfi.IsEmpty)
+            {
+                WriteLogAndConsole("Backup " + localPath.Substring(backupItem.LocalPath.Length) + " Success    ", Y, X);
+            }
+            else
+            {
+                fail++;
+                WriteLogAndConsole("Failed to backup " + localPath.Substring(backupItem.LocalPath.Length) + ": " + pcs.getError() + "    ", Y, X);
+                WriteLogAndConsole(pcs.getRawData(out encode));
+            }
+        }
+
+        /// <summary>
+        /// BaiduPCS_NET.dll 使用的 API 在上传以点号开头的文件时（例如：'.gitignore'），
+        /// 会自动去掉开始的点号（例如：'gitignore'）。
+        /// 因此，我们使用重命名函数修正其文件名。
+        /// </summary>
+        /// <param name="fi">上传后获得的网盘文件元数据</param>
+        /// <param name="remotePath">正确的网盘文件路径</param>
+        /// <returns></returns>
+        protected PcsFileInfo Validate(PcsFileInfo fi, string remotePath)
+        {
+            if (fi.IsEmpty)
+                return fi;
+            string filename1 = Path.GetFileName(remotePath);
+            string filename2 = Path.GetFileName(fi.path);
+            if (filename1 == filename2)
+                return fi;
+            rename_total++;
+            PcsPanApiRes ppar = pcs.rename(new SPair[] {
+                    new SPair(fi.path, filename1)
+                });
+            if (ppar.error == 0)
+            {
+                WriteLogAndConsole("Rename " + fi.path + " to " + filename1.Substring(backupItem.LocalPath.Length) + "    ", Y, X);
+            }
+            else
+            {
+                rename_fail++;
+                WriteLogAndConsole("Failed to rename " + fi.path + " to " + filename1.Substring(backupItem.LocalPath.Length) + " : " + pcs.getError() + "    ", Y, X);
+                WriteLogAndConsole(pcs.getRawData(out encode));
+            }
+            return fi;
+        }
+
+        /// <summary>
+        /// 显示上传进度
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void onProgressChange(object sender, ProgressChangeArgs e)
+        {
+            if (e.size <= 0)
+                return;
+            int percentage = (int)((e.finished * 10000) / e.size);
+            string percentage_msg = Utils.HumanReadableSize(e.finished)
+                + "/" + Utils.HumanReadableSize(e.size)
+                + "  " + ((float)percentage / 100.0f).ToString("F2") + "%";
+            WriteConsole(percentage_msg + "    ", Y + 1, X);
+        }
+
+        private void onUploadSliceError(object sender, SliceErrorArgs e)
+        {
+            WriteLogAndConsole(e.errmsg + "\r\n" + e.raw + "\r\n" + (e.exception != null ? e.exception.Message + "\r\n" + e.exception.StackTrace : "") + "    ", Y + 1, X);
+            e.cancelled = false; //重试上传分片
+        }
+
+        protected virtual void WriteLogAndConsole(string line, int y = -1, int x = -1)
+        {
+            WriteConsole(line, y, x);
+            WriteLog(line);
+        }
+    }
+}
---
+++ Sample/Sample_1_FileBackup/Backup/SecureUploader.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.IO;
+
+using BaiduPCS_NET;
+using BaiduPCS_NET.Native;
+
+namespace FileBackup
+{
+    public class SecureUploader : Uploader
+    {
+        /// <summary>
+        /// 加密器
+        /// </summary>
+        public IBlockEncoder Encoder { get; set; }
+
+        public SecureUploader()
+            : base()
+        { }
+
+        public SecureUploader(BaiduPCS pcs, string slice_dir, IBlockEncoder encoder)
+            : base(pcs, slice_dir)
+        {
+            Encoder = encoder;
+        }
+
+
+        /// <summary>
+        /// 执行快速上传
+        /// </summary>
+        /// <param name="localPath">文件的本地绝对路径</param>
+        /// <param name="remotePath">文件的网盘绝对路径</param>
+        /// <param name="overwrite">如果网盘文件已经存在，是否覆盖原文件。true - 覆盖；false - 自动重命名</param>
+        /// <param name="remoteFileInfo">上传成功后，此变量被设置为网盘中文件的元数据</param>
+        /// <param name="filemd5">出入的文件的MD5值，如果没有传入，且内部需要，则会自动计算</param>
+        /// <returns>返回是否上传成功</returns>
+        protected override bool RapidUpload(string localPath, string remotePath, bool overwrite, out PcsFileInfo remoteFileInfo, out string fileMd5)
+        {
+            string slicemd5;
+            byte[] slice = new byte[NativeConst.PCS_RAPIDUPLOAD_THRESHOLD];
+            int len;
+            fileMd5 = null;
+            using (FileStream fs = new FileStream(localPath, FileMode.Open, FileAccess.Read, FileShare.Read, 1024))
+            {
+                len = fs.Read(slice, 0, slice.Length);
+            }
+            //文件读取错误
+            if (len != NativeConst.PCS_RAPIDUPLOAD_THRESHOLD)
+            {
+                remoteFileInfo = new PcsFileInfo();
+                return false;
+            }
+            slice = Encoder.Encode(slice, 0, slice.Length);
+            slicemd5 = NativeUtils.pcs_md5(slice);
+
+            // 计算 MD5 
+            FileStream stream = new FileStream(localPath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096);
+            if (!pcs.md5(new OnReadBlockFunction(OnReadBlock), stream, out fileMd5))
+            {
+                stream.Close();
+                remoteFileInfo = new PcsFileInfo();
+                return false;
+            }
+            stream.Close();
+
+            remoteFileInfo = pcs.rapid_upload(remotePath, localPath, ref fileMd5, ref slicemd5, overwrite); //快速上传
+            if (!remoteFileInfo.IsEmpty) //上传成功，则直接返回
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// 直接上传
+        /// </summary>
+        /// <param name="localPath">文件的本地绝对路径</param>
+        /// <param name="remotePath">文件的网盘绝对路径</param>
+        /// <param name="overwrite">如果网盘文件已经存在，是否覆盖原文件。true - 覆盖；false - 自动重命名</param>
+        /// <param name="remoteFileInfo">上传成功后，此变量被设置为网盘中文件的元数据</param>
+        /// <param name="filesize">传入的文件大小，如果没有传入，且内部需要，则内部会自动读取</param>
+        /// <param name="filemd5">出入的文件的MD5值，如果没有传入，且内部需要，则会自动计算</param>
+        /// <returns>返回是否上传成功</returns>
+        protected override bool Upload(string localPath, string remotePath, bool overwrite, out PcsFileInfo remoteFileInfo, long filesize = -1, string filemd5 = null)
+        {
+            if (filesize < 0)
+                filesize = GetLocalFileSize(localPath);
+            FileStream stream = new FileStream(localPath, FileMode.Open, FileAccess.Read, FileShare.Read, 4096);
+            remoteFileInfo = pcs.upload_s(remotePath, new OnReadBlockFunction(OnReadBlock), (uint)filesize, stream, overwrite);
+            stream.Close();
+            if (!remoteFileInfo.IsEmpty) //上传成功，则直接返回
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// 获取文件的大小
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns>返回文件的大小</returns>
+        protected override long GetLocalFileSize(string filename)
+        {
+            long rawSize = base.GetLocalFileSize(filename);
+            return Encoder.CalculateSize(rawSize);
+        }
+
+        /// <summary>
+        /// 当需要读取数据块来计算MD5时触发
+        /// </summary>
+        /// <param name="sender">触发此事件的对象</param>
+        /// <param name="buf">读入成功后，此变量中存储读入的块数据</param>
+        /// <param name="size">待读入的元素的字节大小</param>
+        /// <param name="nmemb">一共读入多少个元素</param>
+        /// <param name="userdata"></param>
+        /// <returns>返回实际读入的数据的字节大小</returns>
+        private int OnReadBlock(BaiduPCS sender, out byte[] buf, uint size, uint nmemb, object userdata)
+        {
+            buf = null;
+            FileStream stream = (FileStream)userdata;
+            buf = new byte[size * nmemb];
+            int len = stream.Read(buf, 0, buf.Length);
+            buf = Encoder.Encode(buf, 0, len);
+            return len;
+        }
+
+        /// <summary>
+        /// 当需要读取分片数据时触发
+        /// </summary>
+        /// <param name="sender">触发此事件的对象</param>
+        /// <param name="buf">读入成功后，此变量中存储读入的块数据</param>
+        /// <param name="size">待读入的元素的字节大小</param>
+        /// <param name="nmemb">一共读入多少个元素</param>
+        /// <param name="userdata"></param>
+        /// <returns>返回实际读入的数据的字节大小</returns>
+        protected override int OnReadSlice(BaiduPCS sender, out byte[] buf, uint size, uint nmemb, object userdata)
+        {
+            int len = base.OnReadSlice(sender, out buf, size, nmemb, userdata);
+            if (len > 0 && len != NativeConst.CURL_READFUNC_ABORT && len != NativeConst.CURL_READFUNC_PAUSE)
+                buf = Encoder.Encode(buf, 0, len);
+            return len;
+        }
+
+    }
+}
